9c3a0ccc4853a5dcbb01c86a8c5846c3995ee5de
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-03-22 22:41:06 -0400

    worked on index and show views

--- app/controllers/hearts_controller.rb
+++ app/controllers/hearts_controller.rb
@@ -1,4 +1,16 @@
 class HeartsController < ApplicationController
   def create
+    @heart = Heart.new(heart_params)
+
+    if @heart.save
+      redirect_to @heart.post, notice: "Hearted this post!"
+    else
+      redirect_to root_url, notice: "Oh no! This couldn't be hearted!"
+    end
+  end
+
+  private
+  def heart_params
+    params.require(:heart).permit(:post_id)
   end
 end
